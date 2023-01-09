b9d89c6827111182cd74a83b11fc8019c6f60c55
Shreya Vashisht <svashisht@schubergphilis.com>
2023-01-09 16:28:09 +0100

    Changing back the git source to SBP ones after testing

--- main.tf
+++ main.tf
@@ -32,8 +32,7 @@ module "account" {
 
 module "tfe_workspace" {
   count = var.create_default_workspace ? 1 : 0
-  #  source    = "github.com/schubergphilis/terraform-aws-mcaf-workspace?ref=v0.10.0"
-  source    = "github.com/svashisht03/terraform-aws-mcaf-workspace"
+  source    = "github.com/schubergphilis/terraform-aws-mcaf-workspace?ref=v0.10.0"
   providers = { aws = aws.account }
 
   agent_pool_id                  = var.tfe_workspace.agent_pool_id
@@ -74,8 +73,7 @@ module "tfe_workspace" {
 
 module "additional_tfe_workspaces" {
   for_each = var.additional_tfe_workspaces
-  #source    = "github.com/schubergphilis/terraform-aws-mcaf-workspace?ref=v0.10.0"
-  source    = "github.com/svashisht03/terraform-aws-mcaf-workspace"
+  source    = "github.com/schubergphilis/terraform-aws-mcaf-workspace?ref=v0.10.0"
   providers = { aws = aws.account }
 
   agent_pool_id                  = each.value.agent_pool_id != null ? each.value.agent_pool_id : var.tfe_workspace.agent_pool_id
