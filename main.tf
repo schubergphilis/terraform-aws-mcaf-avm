83f9f0c2681eabf4cdb9ce11856647f11f37ca4f
Shreya Vashisht <svashisht@schubergphilis.com>
2023-01-11 09:18:06 +0100

    changing source back

--- main.tf
+++ main.tf
@@ -46,8 +46,7 @@ resource "aws_iam_policy" "workload_boundary" {
 
 module "tfe_workspace" {
   count = var.create_default_workspace ? 1 : 0
-  #  source    = "github.com/schubergphilis/terraform-aws-mcaf-workspace?ref=v0.10.0"
-  source    = "github.com/svashisht03/terraform-aws-mcaf-workspace"
+  source    = "github.com/schubergphilis/terraform-aws-mcaf-workspace?ref=v0.10.0"
   providers = { aws = aws.account }
 
   agent_pool_id                  = var.tfe_workspace.agent_pool_id
@@ -87,8 +86,7 @@ module "tfe_workspace" {
 
 module "additional_tfe_workspaces" {
   for_each = var.additional_tfe_workspaces
-  #source    = "github.com/schubergphilis/terraform-aws-mcaf-workspace?ref=v0.10.0"
-  source    = "github.com/svashisht03/terraform-aws-mcaf-workspace"
+  source    = "github.com/schubergphilis/terraform-aws-mcaf-workspace?ref=v0.10.0"
   providers = { aws = aws.account }
 
   agent_pool_id                  = each.value.agent_pool_id != null ? each.value.agent_pool_id : var.tfe_workspace.agent_pool_id
