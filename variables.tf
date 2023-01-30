bbb2a48d7a80a09a2c58e8134cd7ec893ce08d79
Sjoerd Tromp <stromp@schubergphilis.com>
2023-01-30 16:47:15 +0100

    Testing

--- variables.tf
+++ variables.tf
@@ -33,7 +33,7 @@ variable "account" {
 variable "additional_tfe_workspaces" {
   type = map(object({
     agent_pool_id                  = optional(string, null)
-    agent_role_arn                 = optional(string, null)
+    agent_role_arns                 = optional(list(string), null)
     auth_method                    = optional(string, null)
     auto_apply                     = optional(bool, false)
     branch                         = optional(string, null)
@@ -114,7 +114,7 @@ variable "permissions_boundaries" {
 variable "tfe_workspace" {
   type = object({
     agent_pool_id                  = optional(string, null)
-    agent_role_arn                 = optional(string, null)
+    agent_role_arns                 = optional(list(string), null)
     auth_method                    = optional(string, "iam_user")
     auto_apply                     = optional(bool, false)
     branch                         = optional(string, "main")
